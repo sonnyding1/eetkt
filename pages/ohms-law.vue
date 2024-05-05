70ab30a5126ef8189d4ee4f772ded358079f569f
Sonny Ding <sonnyding20@gmail.com>
2024-05-05 01:17:17 -0700

    chore: ohms law html

--- pages/ohms-law.vue
+++ pages/ohms-law.vue
@@ -13,14 +13,88 @@
       <li>I is the current</li>
       <li>R is the resistance</li>
     </ul>
-    <InputNumber v-model="voltage" placeholder="Voltage" />
+
+    <p>I want to find:</p>
+    <Dropdown
+      v-model="selected"
+      :options="options"
+      optionLabel="label"
+      placeholder="Select an option"
+    />
+
+    <div class="compute">
+      <InputGroup>
+        <InputNumber
+          v-model="voltage"
+          @input="updateVoltage"
+          placeholder="Voltage"
+          :maxFractionDigits="5"
+        />
+        <InputGroupAddon>V</InputGroupAddon>
+      </InputGroup>
+      <p>=</p>
+      <InputGroup>
+        <InputNumber
+          v-model="current"
+          @input="updateCurrent"
+          placeholder="Current"
+          :maxFractionDigits="5"
+        />
+        <InputGroupAddon>A</InputGroupAddon>
+      </InputGroup>
+      <p>&times;</p>
+      <InputGroup>
+        <InputNumber
+          v-model="resistance"
+          @input="updateResistance"
+          placeholder="Resistance"
+          :maxFractionDigits="5"
+        />
+        <InputGroupAddon>Ω</InputGroupAddon>
+      </InputGroup>
+    </div>
+    <p>{{ selected }}</p>
   </div>
 </template>
 
 <script setup>
 import InputNumber from "primevue/inputnumber";
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import Dropdown from "primevue/dropdown";
+
+import { ref } from "vue";
 
 const voltage = ref(0);
+const current = ref(0);
+const resistance = ref(0);
+
+const options = ref([
+  { label: "Voltage", code: "V" },
+  { label: "Current", code: "I" },
+  { label: "Resistance", code: "R" },
+]);
+
+const selected = ref(null);
+
+const updateVoltage = (e) => {
+  voltage.value = e.value;
+};
+
+const updateCurrent = (e) => {
+  current.value = e.value;
+};
+
+const updateResistance = (e) => {
+  resistance.value = e.value;
+};
 </script>
 
-<style></style>
+<style scoped>
+.compute {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  justify-content: space-between;
+}
+</style>
