a12a9670535cdeca12dfe55006647c73f990f550
Sonny Ding <sonnyding20@gmail.com>
2024-05-04 23:54:17 -0700

    chore: generate ohms law page placeholder

---
+++ pages/ohms-law.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Ohm's Law</h1>
+    <p>
+      Ohm's Law states that the current through a conductor between two points
+      is directly proportional to the voltage across the two points.
+    </p>
+    <p>Ohm's Law is given by the formula:</p>
+    <p>V = IR</p>
+    <p>Where:</p>
+    <ul>
+      <li>V is the voltage</li>
+      <li>I is the current</li>
+      <li>R is the resistance</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
